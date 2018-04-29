ddcada5a7262e03a1e071be8e301d34adefc640f
Alireza Bashiri <azbshiri@gmail.com>
2018-04-29 07:25:38 +0430

    Add cli interface

---
+++ cmd/cmd.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/azbshiri/jhop/api"
+	"github.com/urfave/cli"
+)
+
+func Run(args []string) {
+	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "port",
+			Value: "6000",
+			Usage: "Set port",
+		},
+		cli.StringFlag{
+			Name:  "host",
+			Value: "localhost",
+			Usage: "Set host",
+		},
+	}
+	app.Name = "jhop"
+	app.Usage = "Create fake REST API in one sec."
+	app.Action = func(c *cli.Context) error {
+		var filename string
+		if c.NArg() > 0 {
+			filename = c.Args()[0]
+		}
+		if filename != "" {
+			api.Create(filename, c.String("host"), c.String("port"))
+		}
+		return nil
+	}
+
+	err := app.Run(args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,37 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"os"
 
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
+	"github.com/azbshiri/jhop/cmd"
 )
 
+type cat struct {
+	*cat
+}
+
 func main() {
-	raw, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
-
-	var data map[string]interface{}
-	err = json.Unmarshal(raw, &data)
-	if err != nil {
-		panic(err)
-	}
-
-	r := mux.NewRouter()
-	for k, _ := range data {
-		p := k // why doing this? see https://stackoverflow.com/a/44045012/4794989
-		r.HandleFunc(fmt.Sprintf("/%s", p), func(w http.ResponseWriter, r *http.Request) {
-			json.NewEncoder(w).Encode(map[string]interface{}{p: data[p]})
-		}).Methods("GET")
-	}
-
-	withLogging := handlers.LoggingHandler(os.Stdout, r)
-	log.Fatal(http.ListenAndServe(":6000", withLogging))
+	cmd.Run(os.Args)
 }
