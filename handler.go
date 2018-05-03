76cc2dedb9e8dd8a70379c8ed1a121f5aa6821f8
Roman Budnikov <romanyx90@yandex.ru>
2018-05-03 11:19:43 +0300

    Routes implementation

--- handler.go
+++ handler.go
@@ -13,28 +13,47 @@ import (
 
 // NewHandler generates new http.Handler, which handles
 // resources from given files.
-func NewHandler(rs ...io.Reader) (http.Handler, error) {
+func NewHandler(rs ...io.ReadCloser) (http.Handler, error) {
+	return newHandler(make(map[string]string), rs...)
+}
+
+// NewHandlerWithRoutes generates new http.Handler, which handlers
+// resources from given files and specified routes.
+func NewHandlerWithRoutes(routes map[string]string, rs ...io.ReadCloser) (http.Handler, error) {
+	return newHandler(routes, rs...)
+}
+
+func newHandler(routes map[string]string, rs ...io.ReadCloser) (http.Handler, error) {
 	router := mux.NewRouter()
 	for _, r := range rs {
 		var resources map[string]interface{}
 		if err := json.NewDecoder(r).Decode(&resources); err != nil {
 			return nil, errors.Wrap(err, "unmarshal failed")
 		}
+		r.Close()
 
-		addResource(router, resources)
+		addResource(router, routes, resources)
 	}
 
 	return router, nil
 }
 
-func addResource(r *mux.Router, resources map[string]interface{}) {
+func addResource(r *mux.Router, routes map[string]string, resources map[string]interface{}) {
 	for k := range resources {
 		p := k // why doing this? see https://stackoverflow.com/a/44045012/4794989
-		r.HandleFunc(fmt.Sprintf("/%s", p), func(w http.ResponseWriter, r *http.Request) {
+		indexPath := fmt.Sprintf("/%s", p)
+		if path, ok := routes[indexPath]; ok {
+			indexPath = path
+		}
+		r.HandleFunc(indexPath, func(w http.ResponseWriter, r *http.Request) {
 			json.NewEncoder(w).Encode(map[string]interface{}{p: resources[p]})
 		}).Methods("GET")
 
-		r.HandleFunc(fmt.Sprintf("/%s/{id}", p), func(w http.ResponseWriter, r *http.Request) {
+		showPath := fmt.Sprintf("/%s/{id}", p)
+		if path, ok := routes[showPath]; ok {
+			showPath = path
+		}
+		r.HandleFunc(showPath, func(w http.ResponseWriter, r *http.Request) {
 			switch v := resources[p].(type) {
 			case []interface{}:
 				for _, m := range v {
